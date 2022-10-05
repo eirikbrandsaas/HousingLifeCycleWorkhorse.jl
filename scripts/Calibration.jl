75a1cf0fee7cde19f00a823c0e1d13cc59c3b36b
Eirik Eylands Brandsaas <eebrandsaas@gmail.com>
2022-10-05 16:44:06 -0400

    Can now plot model fit

--- scripts/Calibration.jl
+++ scripts/Calibration.jl
@@ -2,7 +2,6 @@ include("../src/HousingLifeCycleWorkhorse.jl")
 
 # Issues
 # - Start at age 20
-# - Add financial wealth model moments
 
 ## Initialize
 par = benchpar(ns=2,nh=4,nx=70,ms=0.04,mb=0.02)
@@ -18,6 +17,6 @@ po = plot(momg[1].age,momg[1].own*100,label="data",ylabel="%")
 plot!(po,aggmom.age,aggmom.own*100,label="model")
 
 px = plot(momg[1].age,momg[1].finwealth,label="data",ylabel="1000s 1995 USD")
-# plot!(px,aggmom.age,aggmom.finwealth,label="model")
+plot!(px,aggmom.age,aggmom.fx,label="model")
 
 plot(po,px,xlim=(18,50),plot_title="Model Fit",xlabel="Age")
